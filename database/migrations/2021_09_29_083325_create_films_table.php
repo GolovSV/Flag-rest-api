4d4d24d9e725239ed5ba4a72944f31c7c5053bd5
Sergey Golov <golov.developimg@gmail.com>
2021-09-29 15:34:23 +0500

    создал миграции

---
+++ database/migrations/2021_09_29_083325_create_films_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('year')->nullable();
+            $table->string('country')->nullable();
+            $table->foreignId('genre_id')->nullable()->references('id')->on('genres')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
