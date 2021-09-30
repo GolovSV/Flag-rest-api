bcde2563b41b6993240b2c2984fe104a9d768094
Sergey Golov <golov.developimg@gmail.com>
2021-09-30 16:56:47 +0500

    update actor seeder

--- database/seeders/ActorSeeder.php
+++ database/seeders/ActorSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Actor;
+use App\Models\Film;
 use Illuminate\Database\Seeder;
 
 class ActorSeeder extends Seeder
@@ -14,8 +15,21 @@ class ActorSeeder extends Seeder
      */
     public function run()
     {
-        Actor::factory()
+        $actors = Actor::factory()
             ->count(50)
             ->create();
+
+        foreach ($actors as $actor) {
+            $arr = $this->random_array(rand(1, 3), 1, rand(1, Film::count()));
+            $actor->films()->sync($arr);
+        }
+
+    }
+
+    function random_array($n, $min, $max)
+    {
+        return array_map(function () use ($min, $max) {
+            return mt_rand($min, $max);
+        }, range(1, $n));
     }
 }
