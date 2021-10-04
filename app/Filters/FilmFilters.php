c35b87097c745ea6257002c59f85bc06a2cd590f
Sergey Golov <golov.developimg@gmail.com>
2021-10-04 22:56:21 +0500

    film filters were refactored by whereRelation() method

--- app/Filters/FilmFilters.php
+++ app/Filters/FilmFilters.php
@@ -8,19 +8,12 @@ class FilmFilters extends QueryFilter
 {
     public function genre($parameter)
     {
-        return $this->builder
-            ->LeftJoin('genres', 'films.genre_id', '=', 'genres.id')
-            ->select('films.*')
-            ->where('genres.title', 'like', "%$parameter%");
+        return $this->builder->whereRelation('genre', 'title', 'like', "%$parameter%");
     }
 
     public function actor($parameter)
     {
-        return $this->builder
-            ->join('actor_film', 'films.id', '=', 'actor_film.film_id')
-            ->join('actors', 'actor_film.actor_id', '=', 'actors.id')
-            ->select('films.*')
-            ->where('actors.name', 'like', "%$parameter%");
+        return $this->builder->whereRelation('actors', 'name', 'like', "%$parameter%");
     }
 
     public function sort($order = 'asc')
